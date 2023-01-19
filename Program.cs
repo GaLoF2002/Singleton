559538afc3bf741b2ffb08610528fc23327bda53
Galo Andres Estrella Jimenez <galo.estrella.jimenez@udla.edu.ec>
2023-01-19 08:41:31 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace RefactoringGuru.DesignPatterns.Singleton.Conceptual.NonThreadSafe
+{
+
+    public sealed class Singleton
+    {
+
+        private Singleton() { }
+
+     
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton trabaja correctamente, variables contienen misamas isntncias");
+            }
+            else
+            {
+                Console.WriteLine("Singleton fallo, variables contienen diferente instancias.");
+            }
+        }
+    }
+}
+
